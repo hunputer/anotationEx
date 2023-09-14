02fe6c4933da275f9dd51723b8b1aa8c92d5074b
hunputer <wlgns3855@gmail.com>
2023-09-14 22:57:48 +0900

    @bean2

--- src/main/java/com/example/anotationEx/Controller/MainController.java
+++ src/main/java/com/example/anotationEx/Controller/MainController.java
@@ -1,5 +1,9 @@
 package com.example.anotationEx.Controller;
 
+import com.example.anotationEx.vo.MainVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/main")
 public class MainController {
 
+    @Autowired
+    private MainVO mainVO;
+
     @GetMapping("")
     public void main(){
-        System.out.println("ㅎㅇ");
+        System.out.println(mainVO.getId());
     }
 
 }
--- src/main/java/com/example/anotationEx/vo/MainVO.java
+++ src/main/java/com/example/anotationEx/vo/MainVO.java
@@ -1,4 +1,14 @@
 package com.example.anotationEx.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
 public class MainVO {
+
+    private String id;
+    private String name;
+
 }
